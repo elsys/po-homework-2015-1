24ca52ea6716bf6d5db30530f8375c844973e7bd
mstanimirov <marto0799@gmail.com>
2015-10-07 09:40:34 +0300

    Homework finished

--- 2015-2016/G/24/02/repeat.c
+++ 2015-2016/G/24/02/repeat.c
@@ -4,44 +4,55 @@
 
 int main(){
 
-	char s[100];
-	int i,c;
-	bool check = false;
+	int d[100];
+	int i,j,a,b,n,count=1;
 
-	do{
+	scanf("%d", &n);
 
-		scanf("%s", s);		
+	for(i=0;n!=0;i++){
 
-		for(i=strlen(s);i>=3;i--){
+		d[i] = n%10;
+		n/=10;
 
-			if(s[i]=='P' && s[i-1]=='O' && s[i-2]=='T' && s[i-3]=='S' ){
+	}
 
-				check = true;
+	n = i;
+
+	for(i=0;i<n;i++){
+
+		for(b=n-1;b>i;b--){
+			
+			if(d[b-1]>d[b]){
+	
+				a=d[b-1];
+				d[b-1]=d[b];
+				d[b]=a;	
+			}
+		}
+
+	}
+
+	for(i=0;i<n;i++){
+
+		for(b=i+1;b<n;b++){
+			
+			if(d[i]==d[b]){
+				
+				count++;
+				
+			}else{
+
+				break;
 			
 			}
-		
-		}
-
-
-		for(i=0;i<=strlen(s);i++){
-		      
-			if(s[i]>='a' && s[i]<='z'){
-
-				s[i]=s[i]-32;
-
-			}
-		       
-		}
-
-		printf("%s \n", s);
-
-		if(check){
-
-			break;
 
 		}
-	
-	}while(1);
+
+		printf("\n %d - %d \n", d[i], count);
+		i = b - 1;
+		count = 1;
+
+	}
 
 	return 0;
 

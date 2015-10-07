e8aa17d10b048375a37480a4be2d28b2c4df4f36
Atira12 <tigerpaw@abv.bg>
2015-10-07 10:55:51 +0300

    homework 03 number in class :3

---
+++ 2015-2016/G/3/03/stop.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+char words[100];
+int i=0;
+
+do{
+scanf("\n%s",words);
+if(words[0] == 'S' && words[1] == 'T' && words[2] == 'O' && words[3] == 'P'){
+break;
+}
+
+else
+{
+int f=0;
+for(;f<100;f++)
+{
+if(words[f]>='a'&& words[f]<='z')
+
+{
+words[f]-=32;
+
+}
+
+
+}
+printf("\n%s",words);
+}
+
+}while(1);
+
+return 0;
+}

e92308ab0a76680ddf8e65dbb44f1126495f80bd
David-Nicollas <david.nicolas06.99@Gmail.com>
2015-10-11 21:42:36 +0300

    Triugulnik

---
+++ 2015-2016/G/09/03/Triugulnik.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+
+	printf("Please enter n (n is the height of the triangle)");
+	scnaf("%d", &n);
+
+	printf("Osnovata e: %d" , ((-2)*n - 1))
+	pirntf("Visochinata e: "%d", n);
+	return 0;
+
+}

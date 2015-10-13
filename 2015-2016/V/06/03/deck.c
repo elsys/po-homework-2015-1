5f92e70beca4892bdfbdf4ce41b11f460948a70c
bojan-dim <bojan_dim@abv.bg>
2015-10-13 23:22:31 +0300

    Deck.c с buble sort

--- 2015-2016/V/06/03/deck.c
+++ 2015-2016/V/06/03/deck.c
@@ -1,42 +1,51 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-   char a[13]; int b,i;
-   printf("Vav. teste:");
-   for(i=0;i<13;i++){
-     b=getchar();
-     if(b>'1' && b<='9')
-        a[i]=(int)b-48;
-     else
-      switch(b){
-        case 'T': a[i]=10; break;
-        case 'J': a[i]=11; break;
-        case 'Q': a[i]=12; break;
-        case 'K': a[i]=13; break;
-        case 'A': a[i]=14; break;
-        }//switch input;
-   }//for input;
-
-     int l=2,s=0;
-  for(i=0;i<13;i++){
-     for(s=0;i<13;s++);{
-        if(a[s]==l){
-          a[s]==l;
-          break; 
-         }
-         l++; 
-      }//for1
-       if(l>=10 && l<=14)
-         switch(l){
-          case 10: printf("T"); break;
-          case 11: printf("J"); break;
-          case 12: printf("Q"); break;
-          case 13: printf("K"); break;
-          case 14: printf("A"); break;
-        }//switch;
-       else
-         printf("%d",l); 
-   }//for
-
-}//main
+int main()
+{
+	int a[13],i,sort=0,n=13,b;
+	char x;
+	printf("\nVav.teste:");
+	for(i=0;i<13;i++)
+	{
+		x=getchar();
+		if(x<='9' && x>'1')
+			a[i]=(int)x-48;
+		else
+			switch(x)
+			{
+				case 'T': a[i]=10; break;
+				case 'J': a[i]=11; break;
+				case 'Q': a[i]=12; break;
+				case 'K': a[i]=13; break;
+				case 'A': a[i]=14; break;
+			}
+	}
+	while(!sort)
+	{
+		sort=1;
+		for(i=0;i<n;i++)
+			if(a[i]>a[i+1])
+			{
+				sort=0;
+				b=a[i];
+				a[i]=a[i+1];
+				a[i+1]=b;
+			}
+		n--;
+	}
+	for(i=0;i<13;i++)
+	{
+		if(a[i]>=2 && a[i]<=10)
+			printf("%d ",a[i]);
+		else
+			switch(a[i])
+			{
+				case 11: printf("J "); break;
+				case 12: printf("Q "); break;
+				case 13: printf("K "); break;
+				case 14: printf("A "); break;
+			}
+	}
+	printf("\n");
+}

696ef493665ff5603d3612a20d00a26744941eb0
Luchezar Lubenov <csgeimura@abv.bg>
2015-10-13 20:17:35 +0300

    hello.c

---
+++ 2015-2016/G/22/03/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+char n[100][200];
+int i;{
+printf("Vuvedete duma:");
+}
+scanf( "%s",n[0]);
+for(i=4;i>=0;i--)
+{
+printf( "%c", n[0][i]);
+}
+printf( "\n" );
+}

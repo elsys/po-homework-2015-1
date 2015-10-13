5c91007acfae39cf7af512ca1ec86b46ff335738
DeyanHristov <Deyan.Xristov@gmail.com>
2015-10-13 22:31:21 +0300

    Create Cards.c

---
+++ 2015-2016/V/09/03/Cards.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+	char cards[100];
+	int i,j, temp, sort;
+	printf("Enter 13 cards in a random order, from 2 to A:(10=T) \n");
+	for(i = 0 ; i < 13 ; i++){
+		scanf("%c",&cards[i]);
+		scanf("%c",&temp);
+		switch(cards[i]){                   // Converting from letters to numeral values for sorting
+		    case 'T': cards[i]=100; break;   // Because "10" screws up the sorting
+			case 'J': cards[i]=101; break;
+			case 'Q': cards[i]=102; break;
+			case 'K': cards[i]=103; break;
+			case 'A': cards[i]=104; break;
+		}
+	}
+	for(i = 0 ; i < 13 ; i++){              // Sorting
+		for(j = 0; j < 13 ; j++){
+			if(cards[i]<cards[j]){
+                sort = cards[j];
+				cards[j] = cards[i];
+				cards[i] = sort;
+			}
+		}
+	}
+	for(i = 0 ; i < 13 ; i++){
+		switch(cards[i]){                //Converting back into letters for the print
+		    case 100: cards[i] ='T'; break;
+			case 101: cards[i]='J'; break;
+			case 102: cards[i]='Q'; break;
+			case 103: cards[i]='K'; break;
+			case 104: cards[i]='A'; break;
+		}
+		printf("%c",cards[i]);
+	}
+}

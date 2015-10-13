d4eb9d6fe843af4c92ab150ac58fa7e5a821d31a
DeyanHristov <Deyan.Xristov@gmail.com>
2015-10-13 22:59:26 +0300

    Update Cards.c

--- 2015-2016/V/09/03/Cards.c
+++ 2015-2016/V/09/03/Cards.c
@@ -7,11 +7,12 @@ int main()
 	while(i<13){
 		scanf("%c",&cards[i]);
 		scanf("%c",&temp);
-		i++;
-	}
+        if(cards[i] >= '2' && cards[i] <= '9') i++;
+        if(cards[i] == 'A' || cards[i] == 'J' || cards[i] == 'Q' || cards[i] == 'K' || cards[i] == 'A') i++;
+}
 	for(i = 0 ; i < 13 ; i++){
 		switch(cards[i]){                   // Converting from letters to numeral values for sorting
-		    	case 'T': cards[i]=100; break;   // Because "10" screws up the sorting
+		    case 'T': cards[i]=100; break;   // Because "10" screws up the sorting
 			case 'J': cards[i]=101; break;
 			case 'Q': cards[i]=102; break;
 			case 'K': cards[i]=103; break;
@@ -21,7 +22,7 @@ int main()
 	for(i = 0 ; i < 13 ; i++){              // Sorting
 		for(j = 0; j < 13 ; j++){
 			if(cards[i]<cards[j]){
-                		sort = cards[j];
+                sort = cards[j];
 				cards[j] = cards[i];
 				cards[i] = sort;
 			}
@@ -29,7 +30,7 @@ int main()
 	}
 	for(i = 0 ; i < 13 ; i++){
 		switch(cards[i]){                //Converting back into letters for the print
-		    	case 100: cards[i] ='T'; break;
+		    case 100: cards[i] ='T'; break;
 			case 101: cards[i]='J'; break;
 			case 102: cards[i]='Q'; break;
 			case 103: cards[i]='K'; break;

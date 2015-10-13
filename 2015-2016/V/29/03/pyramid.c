75895d54b8c47f37e008eac538d8de26893348cb
philipahristoff <filipahristova@yahoo.com>
2015-10-13 23:16:14 +0300

    The cards program is hella confusing tbh

---
+++ 2015-2016/V/29/03/pyramid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int i,space,rows,k=0;
+    printf("Enter the number of rows: ");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;++i)
+    {
+        for(space=1;space<=rows-i;++space)
+        {
+           printf("  ");
+        }
+        while(k!=2*i-1)
+        {
+           printf("* ");
+           ++k;
+        }
+        k=0;
+        printf("\n");
+    }
+    return 0;
+}

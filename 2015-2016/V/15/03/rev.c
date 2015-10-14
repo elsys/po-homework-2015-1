c417778dd3c5cf0b5a3da2e4e769d12391d1ba01
LilianVachkov <lilvachkov@gmail.com>
2015-10-14 20:40:38 +0300

    Reverse a string changed

--- 2015-2016/V/15/03/rev.c
+++ 2015-2016/V/15/03/rev.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void reverse();
+//void reverse();
+void reverse(char*);
 
 int main() {
 
-	reverse();
+//	reverse();
+	
+	char* input = NULL;
+	scanf("%m[^\n]s", &input);
+
+	reverse(input);
+
+	free(input);			
+	
 	printf("\n");
 	return 0;
 
 }
+
+void reverse(char * string)
+{
+	if((string == NULL) || (*string==0) || !(string))
+		return;
+	unsigned int size = strlen(string) - 1, mid = size/2, beg = 0;
+
+	while((mid--) != 0) {
+
+		string[beg] ^= string[size];
+		string[size] ^= string[beg];
+		string[beg++] ^= string[size--];		
+	
+	}
+		
+	printf("%s", string);
+}
+
+/*
 
 void reverse() {
 
@@ -19,3 +49,5 @@ void reverse() {
 	reverse();
 	printf("%c", ch);
 }
+
+*/

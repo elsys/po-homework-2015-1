5db8a94ece7d1644c9793b0fde748d8e7dd67003
Ivaza <iva.v.zahova@gmail.com>
2015-10-15 17:59:17 +0300

    Homework 2 Programi Iva

---
+++ 2015-2016/A/12/02/1ascii.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(void)
+{
+   int i=0, n=0;
+   for(i=0;i<256;i++)
+   {
+       n++;
+       if(n>25)
+       {
+           n=0; printf("\nPress a to continue");
+           getch();
+       }
+       printf("\n%d %c",i,i);
+   }
+}

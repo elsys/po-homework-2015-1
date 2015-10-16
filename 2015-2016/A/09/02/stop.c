0a3bf757b96a70e7e1eb548c105a6c62c8323a87
Dimitar Nikolov <dimitar@doonloo.com>
2015-10-17 00:36:03 +0300

    Fix stop.c

--- 2015-2016/A/09/02/stop.c
+++ 2015-2016/A/09/02/stop.c
@@ -9,7 +9,7 @@ int main()
 
     while (1)
     {
-        gets(sentence);
+        scanf("%s", sentence);
         if (sentence[0] == 'S' && sentence[1] == 'T' && sentence[2] == 'O' && sentence[3] == 'P')
         {
             return 0;

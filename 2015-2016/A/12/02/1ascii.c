008efff4f8af48361e14bceee8e924aaf2188e00
Ivaza <iva.v.zahova@gmail.com>
2015-10-17 15:02:07 +0300

    1 ASCII fix
    Osuznah 4e conio.h sigurno nqma da trugne na Linux i prosto go napravih
    po-prosto. :p

--- 2015-2016/A/12/02/1ascii.c
+++ 2015-2016/A/12/02/1ascii.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 void main(void)
 {
    int i=0, n=0;
    for(i=0;i<256;i++)
    {
        n++;
-       if(n>25)
-       {
-           n=0; printf("\nPress a to continue");
-           getch();
-       }
        printf("\n%d %c",i,i);
    }
 }

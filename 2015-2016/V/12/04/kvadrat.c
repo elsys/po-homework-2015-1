2e81770c8f570056540100c313bd010a0a3c8ced
Ivan <ivanmilevtues@gmail.com>
2015-10-18 00:07:13 +0300

    Update kvadrat.c

--- 2015-2016/V/12/04/kvadrat.c
+++ 2015-2016/V/12/04/kvadrat.c
@@ -16,22 +16,22 @@ int main(){
 	sum = kvadrat[0][0] + kvadrat[0][1] + kvadrat[0][2];
 	for(i = 0 ; i < 3 ; i++){//proverqva sumata na reda
 		if(sum != kvadrat[i][0] + kvadrat[i][1] + kvadrat[i][2]){
-			printf("No.1 \n");
+			printf("No \n");
 			return 0;
 		}
 	}
 	for(i = 0 ; i < 3 ; i++){//proverqva sumata na reda
 		if(sum != kvadrat[0][i] + kvadrat[1][i] + kvadrat[2][i]){//vertikalni
-			printf("No.2\n");
+			printf("No\n");
 			return 0;
 		}
 	}
 	if(sum != kvadrat[0][0] + kvadrat[1][1] + kvadrat[2][2]){//diagonal
-		printf("No.3\n");
+		printf("No\n");
 		return 0;
 	}
 	if(sum != kvadrat[0][2] + kvadrat[1][1] + kvadrat[2][0]){//diagonal
-		printf("No.4\n");
+		printf("No\n");
 		return 0;
 	}
 	printf("Yes.\n");

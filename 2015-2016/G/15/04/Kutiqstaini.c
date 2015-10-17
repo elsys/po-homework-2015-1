3da7716c8bcd26cf9516a1bc3c71f0b1bdf1353a
EvtimPavlov <evtim_pavlov@abv.bg>
2015-10-17 13:33:09 +0300

    Homework 4

---
+++ 2015-2016/G/15/04/Kutiqstaini.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int func(int n,int square[]);
+int main(){
+int square[50];
+int n,i;
+scanf("%d", &n);
+
+
+	
+	for(i=0;i<n*n;i++){
+	scanf("%d",&square[i]);
+	
+
+
+	}
+
+	if(func(n,square)){
+	printf("magic \n");
+}else{ printf("not magic \n");
+}
+
+return 0;
+
+}
+
+int func(int n,int square[]){
+int i,a,b,j;
+a=0;
+b=0;
+	for(i=0;i<n;i++){
+	a=a+square[i];
+	}
+
+		for(i=0;i<n*n;i=i+n){
+			for(j=i;j<n;j++){
+			b=b+square[j];
+			}
+		if(a!=b){
+		return 0;
+		}
+		}	
+		b=0;	for(i=0;i<n;i++){
+				for(j=i;j<n*n;j=j+n){
+				b=b+square[j];
+				}
+			if(a!=b){
+			return 0;
+			}
+			}
+				
+return 1;
+}

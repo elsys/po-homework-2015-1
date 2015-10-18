1410a7800a040b37a514da38608ea39e0b2c38f5
Mariqn Girginov <mariqn.girginov@gmail.com>
2015-10-18 16:59:18 +0300

    Added digits.c output code

---
+++ 2015-2016/V/16/04/digits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	int d=0;
+	int number;
+	int t;
+	int temp;
+	printf("Enter number:\n");
+	scanf("%d", &number);
+	while (d <= 9){
+		t = 0;
+		temp = number;
+		while(temp > 0){
+			if(temp % 10 == d)
+				t++;
+			temp /= 10;
+		}
+		if (t != 0)
+			printf("The number %d is met %d times\n", d, t);
+		d++;
+	}
+
+}

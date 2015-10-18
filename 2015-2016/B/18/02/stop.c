af02940b09ad8b5084d774996cd7a442db24ada9
LMladenov <lachomladenov@gmail.com>
2015-10-18 19:12:03 +0300

    Update stop.c

--- 2015-2016/B/18/02/stop.c
+++ 2015-2016/B/18/02/stop.c
@@ -5,24 +5,29 @@ int main()
 {
     char word[50];
     int counter, lenght;
-    
+
     scanf("%s", word);
     lenght = strlen(word);
 
     for(counter = 0; counter < lenght; counter++)
     {
-	if(word[counter] == 'S' && word[counter + 1] == 'T' && word[counter + 2] == 'O' && word[counter + 3] == 'P')
-	{
-            
-	    for(counter = 0; counter < (lenght - 4); counter++)
-	    {
-		printf("%c", word[counter] - 32);
-	    }
-	    
- 	    printf("\n");
+        if(word[counter] == 'S' && word[counter + 1] == 'T' && word[counter + 2] == 'O' && word[counter + 3] == 'P')
+        {
+            for(counter = 0; counter < (lenght - 4); counter++)
+            {
+                printf("%c", word[counter] - 32);
+            }
 
-	    break;
-	}
+            printf("\n");
+
+            break;
+        }
+
+        else
+        {
+            printf("Enter word ending with STOP!\n");
+            break;
+        }
     }
 }
 

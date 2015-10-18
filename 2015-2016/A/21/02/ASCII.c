070c7b47aa296f50249258bbeb226b9adb8fd745
FarbyPeckers <mihailernandes@gmail.com>
2015-10-18 14:03:51 +0300

    Homework number 2

---
+++ 2015-2016/A/21/02/ASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int i;
+	for(i=0;i<=255;i++)
+	{
+		printf("\n %d %c ",i,i);
+	}
+}
+	
---
+++ 2015-2016/A/21/02/numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+    int i,n,num[10]={0};
+    scanf("%d",&n);
+    while(n>0)
+    {
+        if(n%10==0)
+        {
+            num[0]++;
+            n=n/10;
+        }
+        else
+        {
+
+        num[n%10]++;
+        n=n/10;
+        }
+    }
+    for(i=0;i<=9;i++)
+    {
+        printf("\n The number %d is showed %d time/s.",i,num[i]);
+    }
+
+}

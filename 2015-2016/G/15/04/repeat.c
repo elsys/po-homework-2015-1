9bc2c8f9a441b3c8231b9503e13a53942807cc57
EvtimPavlov <evtim_pavlov@abv.bg>
2015-10-19 21:53:56 +0300

    Homework 4

--- 2015-2016/G/15/04/repeat.c
+++ 2015-2016/G/15/04/repeat.c
@@ -2,29 +2,64 @@
 #include <string.h>
 
 int main(){
-int repeat[50];
-int n,i,a,b;
+int repeat[10];
+int n,i,a,b,c;
 scanf("%d",&n);
-
-	for(i=0;n!=0;i++){
-	repeat[i]=n%10;
-	n=n/10;
-
-
-
+	c = n;
+	for(i=0;c!=0;i++){
+	c=c/10;
+	
+	}
+	while(i>9){
+	printf("Not valid int \n");
+	scanf("%d",&n);
+	c = n;
+	for(i=0;c!=0;i++){
+	c=c/10;
+	
+	}
 
 	}
-		for(b=0;b<i;b++){
 
-			for(a=b+1;a<i;a++){
+	for(i=0;n!=0;i++){
+	a = n%10;
+	repeat[i] = a;
+	n=n/10;
+	
+	}
+	n = i;
 
-			if(repeat[b]==repeat[a])
+	for(b=0;b<n;b++){
+
+			for(a=n-1;a>b;a--){
+
+			if(repeat[b-1]>repeat[b]){
+				c = repeat[b-1];
+				repeat[b-1] = repeat[b];
+				repeat[b] = c;
+
+			}
 			
-			printf("%d \n ",repeat[b]);
+			
 
 			}
 		
 
+
+
+		}
+
+	for(b=0;b<n;b++){
+
+			for(a=b+1;a<n;a++){
+
+			if(repeat[b]==repeat[a]){printf("%d \n ",repeat[b]);break;}
+			
+			
+
+			}
+		
+			b = a - 1;
 
 
 		}

6d3289dd079860dab465e5a14b9411dfa41e10d9
Verbo1806 <sashoverbovskiy@gmail.com>
2015-10-19 22:15:38 +0300

    Домашно 2

---
+++ 2015-2016/B/01/02/ASCII.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i=0;
+
+    while(i<255)
+    {
+            printf("%c-%d\n",i,i);
+            i++;
+        }
+        return 0;
+    }
+

b4e67ab6130653712ac0459b99263f4d7b2f70ad
EvgaDobreva <evg.dobrev@gmail.com>
2015-10-19 20:38:38 +0300

    Homework 02

--- 2015-2016/A/10/02/glavnibukvi.c
+++ 2015-2016/A/10/02/glavnibukvi.c
@@ -5,16 +5,17 @@ void main(void){
 char s[80];
 int i=0;
 int m=0;
+do{
+i=0;
 scanf("%s",&s);
 m=strlen(s);
-printf("\n%d",m);
 while(s[i]!=0){
 	printf("\n%d",s[i]);
 	if(s[i]>='a' && s[i]<='z'){
 		s[i]=s[i]-('a'-'A');
-		printf("\n%d",s[i]);
 		i++;
 		}
 	}
 	printf("%s",s);
+}while(strcmp(s,"STOP")!=0);
 }

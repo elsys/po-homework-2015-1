da0c21a94cd0571a63e0f5ccb4f20caf06a1f2f9
tuechki <dtuechki@gmail.com>
2015-10-19 21:38:46 +0300

    change getche to getchar

--- 2015-2016/B/13/02/stop.c
+++ 2015-2016/B/13/02/stop.c
@@ -7,18 +7,19 @@ int main()
     char s[max];
 
     for(i=0;i<max;i++)
-    {    s[i]=getche();
-        if(s[i]=='S') {p=i; p++;s[p]=getche();
-        if(s[p]=='T') {p++;s[p]=getche();}
-        if(s[p]=='O') {p++;s[p]=getche();}
+    {    s[i]=getchar();
+        if(s[i]=='S') {p=i; p++;s[p]=getchar();
+        if(s[p]=='T') {p++;s[p]=getchar();}
+        if(s[p]=='O') {p++;s[p]=getchar();}
         if(s[p]=='P') break; }
     }
 
    
-     printf("\n");
+     
     for(l=i,i=0;i<l;i++)
     { if(s[i]>='a' && s[i]<='z')  s[i]=s[i]-32;
        printf("%c",s[i]); }
+       printf("\n");
 
 return 0;
 

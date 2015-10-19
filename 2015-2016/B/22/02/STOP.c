86b2f88f0688565ccbfcfc55031858d2dfe2557e
nikolatzotchev <ntzotchev@gmail.com>
2015-10-20 01:24:35 +0300

    oprawih go

--- 2015-2016/B/22/02/STOP.c
+++ 2015-2016/B/22/02/STOP.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
@@ -7,10 +8,10 @@
 int main() 
 {
     char c[10000];
-    int a,m=0,l=0;
+    int a,m=0,l=0,k=0;
 
-   fgets(c,10000,stdin);
-   a=sizeof(c);
+   fgets(c,100,stdin);
+   a=strlen(c);
 
   while(m<=a)
 
@@ -19,21 +20,30 @@ int main()
    if(c[m-3]=='s' && c[m-2]=='t' && c[m-1]=='o' && c[m]=='p')
  	{
 
-      		for(l=0;l<m-3;l++) {
+      		for(l=0;l<m-4;l++) {
      			if(c[l]<='z' && c[l]>='a')
     			{
     		  		c[l]=c[l]-32;
+                                
 	       		 }
 
                        printf("%c",c[l]);
     
               	}
+               
+                  
   	 }
-   
+                       
    
   m++;
    
  }
+
+
+for (k=l;k<a;k++)
+{
+   printf("%c",c[k]);
+}
                                                                                                                                                                                                                                                                                             return 0; 
 }
 

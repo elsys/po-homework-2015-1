6eb06241b5479f83f7f0ed10eaf255bf9d813eaa
Georgi <georgigt23@gmail.com>
2015-10-19 11:53:30 +0300

    Update square.c

--- 2015-2016/G/08/04/square.c
+++ 2015-2016/G/08/04/square.c
@@ -1,26 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    int i,n,k;
-    int cifri[9] = {0};
-    printf("vuvedi chislo");
-    scanf("%d",&n);
 
-    while(n>0){
+int z=1,n,m,s,i,sb=0,sb2=0,k;
 
-        k=n%10;
+printf("vuvedi strana ");
+scanf("%d",&n);
 
-        cifri[k]++;
+s=n*n;
 
-        n=n/10;
+int arr[s];
+
+m=n*2;
+
+ for(i=0; i<s; i++)
+    {
+
+        scanf("%d",&arr[i]);
+
+        if((i+1)%n==0 && i<s-1) printf("new row\n");
 
     }
 
- for(i=1;i<=9;i++)
-    if(cifri[i]>1)
-    printf("cifrata %d se sreshta %d puti \n",i , cifri[i]);
+    for(i=0;i<n;i++)
+        {
 
- return 0;
+        sb=sb+arr[i];
+
+    }
+i=n;
+    while(i<s){
+
+        for(;i<m;i++)sb2=sb2+arr[i];
+
+        if(sb2!=sb){printf("greshka"); z=0;}
+m=m+n;
+
+sb2=0;
+    }
+if(z)
+     for(i=1; i<n; i++)
+        {
+for(k=i,sb2=0; k<s ;k=k+n){
+
+    if(k<=s)sb2=sb2+arr[k];
+
+}
+
+  if(k>=s && sb2!=sb ){  printf("greshka"); break;}
+
+
+if(i==n-1 && sb2==sb) printf("kvadrata e magicheski");
+
+}
+
+
+    return 0;
+
 }

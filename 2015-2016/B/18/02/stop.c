a97ce8114e6bb1225ecffd1fecc61c4219d0e780
LMladenov <lachomladenov@gmail.com>
2015-10-19 21:25:38 +0300

    Update stop.c
    При мен си работи. Както и да е. 
    Дано с това решение да нямате проблеми.

--- 2015-2016/B/18/02/stop.c
+++ 2015-2016/B/18/02/stop.c
@@ -6,24 +6,19 @@ int main()
     char word[50];
     int counter = 0;
     int lenght = 0;
-    int check = 0;
 
-    do{
-        scanf("%s", word);
-        lenght = strlen(word);
+    scanf("%[^'STOP']s", word);
+    lenght = strlen(word);
 
-        for(counter = 0; counter < lenght; counter++)
+    for(counter = 0; counter < lenght; counter++)
+    {
+        if(word[counter] == 'S' && word[counter + 1] == 'T' && word[counter + 2] == 'O' && word[counter + 3] == 'P')
         {
-            if(word[counter] == 'S' && word[counter + 1] == 'T' && word[counter + 2] == 'O' && word[counter + 3] == 'P')
-            {
-                check = 1;
-                break;
-            }
+            break;
         }
+    }
 
-    }while(!check);
-
-    for(counter = 0; counter < (lenght - 4); counter++)
+    for(counter = 0; counter < lenght - 1; counter++)
     {
         printf("%c", word[counter] - 32);
     }

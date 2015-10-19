fce201e9ba642ba813624f4947729b85539bf4e0
nexillion <nikolay.n.serafimov@gmail.com>
2015-10-19 10:55:17 -0700

    2ra zadacha 2ro domashno

--- 2015-2016/B/24/02/stop.c
+++ 2015-2016/B/24/02/stop.c
@@ -1,23 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-char words[100];
-int i=0;
-do{
-scanf("\n%s",words);
-if(words[0] == 'S' && words[1] == 'T' && words[2] == 'O' && words[3] == 'P'){
+char c[100];
+for(;1;){
+scanf("\n%s",c);
+if(c[0] == 'S' && c[1] == 'T' && c[2] == 'O' && c[3] == 'P'){
 break;
 }
 else{
-int f=0;
-for(;f<100;f++)
+int f;
+for(f=0;f<100;f++)
 {
-if(words[f]>='a'&& words[f]<='z')
+if(c[f]>='a'&& c[f]<='z')
 {
-words[f]-=32;
+c[f]-=32;
 }
-}printf("\n%s",words);
+}printf("\n%s",c);
 }
-}while(1);
+};
 return 0;
 }

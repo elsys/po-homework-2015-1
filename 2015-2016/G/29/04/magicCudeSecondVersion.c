19e1bd58af84b3c227fc5947e661b8ab1049becc
Philip Pepegov <you@example.com>
2015-10-20 03:04:18 +0300

    Филип Пепегов номер 29 ,10г клас.

---
+++ 2015-2016/G/29/04/magicCudeSecondVersion.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+int MagicCube(int input,int multidimentionalArray[input][input]);
+
+int main()
+{
+    int input;
+    printf("Input Lenght Of the multidimentional array");scanf("%d",&input);
+
+    int multidimentionalArray[input][input];
+    int arrayCounter = 0;
+    for(arrayCounter = 0; arrayCounter < input; arrayCounter++)
+    {
+        int secondCounter = 0;
+        for(secondCounter = 0; secondCounter < input; secondCounter++)
+        {
+            printf("Input Digits -->");
+            scanf("%d",&multidimentionalArray[arrayCounter][secondCounter]);
+            printf("\n");
+        }
+    }
+
+    if(MagicCube(input,multidimentionalArray))
+    {
+        printf("The Cube Is Magical\n");
+    }
+    else
+    {
+        printf("The Cube is Not Magical\n");
+    }
+}
+
+int MagicCube(int input,int multidimentionalArray[input][input])
+{
+    int firstSum = 0;
+    int secondSum = 0;
+    int counter = 0;
+    for(counter = 0;counter < input; counter++)
+    {
+        int SecondCounter;
+        for(SecondCounter = 0;SecondCounter<input;SecondCounter++)
+        {
+            firstSum+=multidimentionalArray[counter][SecondCounter];
+        }
+        if(counter==0)
+        {
+           secondSum = firstSum;
+        }
+       if(firstSum != firstSum)
+       {
+            printf("The cube is not magical");
+            return 0;
+      }
+        firstSum = 0;
+    }
+    firstSum = 0;
+//zanulqva 2-ri put zashtoto izliz ot loopa
+    for(counter=0; counter<input; counter++)
+    {
+        firstSum += multidimentionalArray[counter][counter];
+    }
+   if(firstSum!=firstSum)
+  {
+        printf("The cube is not magical");
+       return 0;
+   }
+    firstSum= 0;
+    int SecondCounter;
+    for(counter=0,SecondCounter=input-1 ;counter< input; counter++,SecondCounter--)
+    {
+        firstSum += multidimentionalArray[counter][SecondCounter];
+    }
+   if(firstSum!=firstSum)
+   {
+        printf("The cube is not magical");
+        return 0;
+  }
+    return 1;
+}
+
+

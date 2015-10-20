498214a94d9a25e314792c71d3e521b69c01d180
Luchezar Lubenov <csgeimura@abv.bg>
2015-10-20 23:22:35 +0300

    repetitive_digits.c

---
+++ 2015-2016/G/22/04/repetitive_digits.c
@@ -0,0 +1,33 @@
+int main()
+{
+   int i, chislo, broi[10];
+
+
+   for (i = 0; i < 10; i++)
+   {
+     broi[i] = 0;
+   }
+
+
+   printf("Vuvedete chislo: ");
+   scanf("%d", &chislo);
+
+
+   while (chislo != 0)
+   {
+     broi[chislo % 10]++;
+      chislo /= 10;
+   }
+
+
+   printf("Povtoreni:\n ");
+   for (i = 0; i < 10; i++)
+   {
+      if (broi[i] > 1)
+      {
+         printf("cifrata %d se povtarq\n",i);
+      }
+   }
+
+   return 0;
+}

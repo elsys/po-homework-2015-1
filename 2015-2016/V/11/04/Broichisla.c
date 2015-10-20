1310b94fa3a7d694f05b1108e4fa9917b9b84630
ivaylodimitrovtues <ivaylodimitrovtues@gmail.com>
2015-10-20 21:58:44 +0300

    Create Broichisla.c

---
+++ 2015-2016/V/11/04/Broichisla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(void)
+{
+  int a,i,c,k=0;
+  scanf("%d",&a);
+  do{
+        c=a;
+        i=0;
+        do{
+            if(k==c%10){
+                i++;
+                }
+            c=c/10;
+    }while(c>=1);
+    if(i>1){
+        printf("%d puti chisloto %d \n",i,k);
+    }
+    k++;
+  }while(k<10);
+}

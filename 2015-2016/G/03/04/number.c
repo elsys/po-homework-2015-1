d0569b46a1327463fdf40b8a5e37c9aad49e919a
Atira12 <tigerpaw@abv.bg>
2015-10-20 23:28:33 +0300

    modify number.c

--- 2015-2016/G/03/04/number.c
+++ 2015-2016/G/03/04/number.c
@@ -3,35 +3,60 @@
 
 int main()
 {
-    int n[20];
     int i=0;
-    int number;
-    int counter;
-    int b;
-int    j=0;
-    printf("enter number =");scanf("%d",&number);
-    while(number!=0){
-            n[i]=number%10;
-        number=number/10;
-        i++;
-    }
-    for(counter=0;counter<i;counter++)
-    {
-     if(n[counter]==n[counter+1])
-     {
-       b=n[counter+1];
-        printf("\nnumber is =%d",b);
-     }
+  int n[20] [20];
+  int check;
+  int boss=0;
+  int  choice=0;
+  int rows;
+  int box=0;
+  int box2=0;
+  int base=0;
+int b=0;
+int k=0;
 
 
+  printf("enter number of rows=");scanf("%d",&rows);
+   for(i=0;i<rows;i++)
+   {
 
-    }
-
-for(j=0;j<=counter;j++)
-{
-
-
+       for(choice=0;choice<rows;choice++){
+            printf("\n enter number %d=",i);
+       scanf("\t%d",&n[i][choice]);
 }
+
+   }
+   for (i = 0; i < rows; i++) {
+      printf("\n");
+      for (choice = 0; choice < rows; choice++) {
+         printf("   %d", n[i][choice]);
+      }
+   }
+
+     for (choice = 0; choice < rows-2; choice++){
+      for (b = 0; b< rows-2; b++){
+            boss = boss + n[b][choice];
+      }
+      }
+for (b = 2; b< rows;b++) {
+      for (choice = 2; choice< rows; choice++) {
+            box = box + n[b][choice];
+      }
+      }
+      if(box==boss)
+      {
+          base=2;
+      }    else{base=1;}
+
+
+
+
+      if(base==2){
+        printf("\nvalid");
+      }
+      else printf("\n Invalid square");
+
+
 
     return 0;
 }

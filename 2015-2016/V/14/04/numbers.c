6a47e0a15ba709eb3764690bc538eeea370dcc8f
Izikatkakek <Joankosev@gmail.com>
2015-10-20 22:40:11 +0300

    added more homework

---
+++ 2015-2016/V/14/04/numbers.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i[50];
+    int number, h, w, d, p;
+
+    printf ("Enter number:\n");
+    scanf("%d", &number);
+    printf ("Number - Times\n");
+
+    for(h=0; number>=1; h++)
+    {
+        i[h] = number % 10;
+        number = number / 10;
+    }
+    for(d=0; d<10; d++)
+    {
+        p = 0;
+        w = h;
+
+    while(w >= 0)
+    {
+        if(d == i[w])
+        p++;
+
+        w--;
+    }
+
+    if(p != 0)
+        printf("%d - x%d\n", d, p);
+
+    }
+ }

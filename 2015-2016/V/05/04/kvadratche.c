9a5152b795879fefc06b78436716817bccfd1a61
Божидар <bojidar.nikoloov@gmail.com>
2015-10-21 10:46:40 +0300

    Update kvadratche.c

--- 2015-2016/V/05/04/kvadratche.c
+++ 2015-2016/V/05/04/kvadratche.c
@@ -15,28 +15,28 @@
  	sum = kvadrat[0][0] + kvadrat[0][1] + kvadrat[0][2];
  	for(a = 0 ; a < 3 ; a++){//proverqva sumata na reda
  		if(sum != kvadrat[a][0] + kvadrat[a][1] + kvadrat[a][2]){
-			printf("No.1 \n");
-			printf("No \n");
+			printf("No its not magical kvadratche.1\n");
+			printf("No its not magical kvadratche \n");
  			return 0;
  		}
  	}
  	for(a = 0 ; a < 3 ; a++){//proverqva sumata na reda
  		if(sum != kvadrat[0][a] + kvadrat[1][a] + kvadrat[2][a]){//vertikalni
-			printf("No.2\n");
-			printf("No\n");
+			printf("No its not magical kvadratche.2\n");
+			printf("No its not magical kvadratche\n");
  			return 0;
  		}
  	}
  	if(sum != kvadrat[0][0] + kvadrat[1][1] + kvadrat[2][2]){//diagonal
-		printf("No.3\n");
-		printf("No\n");
+		printf("No its not magical kvadratche.3 \n");
+		printf("No its not magical kvadratche\n");
  		return 0;
  	}
  	if(sum != kvadrat[0][2] + kvadrat[1][1] + kvadrat[2][0]){//diagonal
 		printf("No.4\n");
 
-		printf("No\n");
+		printf("No its not magical kvadratche\n");
  		return 0;
  	}
- 	printf("Yes.\n");
+ 	printf("Yes it is a magical kvadratche.\n");
  }

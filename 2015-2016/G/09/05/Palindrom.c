3543be7bd08814ee5b6ee9ee02539e9bb944ec4e
David-Nicollas <david.nicolas06.99@Gmail.com>
2015-10-21 21:12:06 +0300

    Palindrom.c

---
+++ 2015-2016/G/09/05/Palindrom.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   char a[100], b[100];
+
+   printf("Vuvedete tekst:\n");
+   gets(a);
+
+   strcpy(b,a);
+   strrev(b);
+
+   if (strcmp(a,b) == 0)
+      printf("1\n");
+   else
+      printf("0\n");
+
+   return 0;
+}

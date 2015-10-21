789b055dfa4de3ded97d4c1ea587afd7a6da89cf
Catishere <viktor.gyoshev@gmail.com>
2015-10-21 23:44:48 +0300

    Низ на обратно

---
+++ 2015-2016/B/07/03/maikati.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char a[99], b[99],i,c;
+    fgets(a, 99, stdin);
+    c = strlen(a);
+    for(i = c-1;i >= 0;i--) {
+        printf("%c", a[i]);
+    }
+    return 0;
+}

e1912d4e97df98784a2d7ecd35d3d3c563d983c9
vincho7012 <mihailovblagovest@gmail.com>
2015-10-21 10:04:23 +0300

    Homework03/01
    homework 1 from 20.10

---
+++ 2015-2016/A/02/03/01/task1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   char c;
+    c='*';
+    int n,i,k,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {for(j=n-i;j>0;j--)
+        printf(" ");
+    for(k=n-i;k<n;k++)
+    {if(k==n-1)
+        printf("%c",c);
+    else
+        printf("%c%c",c,c);
+    }
+        printf("\n");
+
+    }
+    return 0;
+}

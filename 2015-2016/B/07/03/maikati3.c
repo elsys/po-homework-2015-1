ee8db6ce8eb63a4edde9e7a77a8e68205a5f6f90
Catishere <viktor.gyoshev@gmail.com>
2015-10-21 23:46:50 +0300

    Задачата с картите

---
+++ 2015-2016/B/07/03/maikati3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+    int main() {
+        char a[13],b;
+        int i,p;
+        fgets(a, 13, stdin);
+        for(i=0;i<13;i++){
+            if(a[i] == 'T') a[i] = ':';
+            if(a[i] == 'J') a[i] = ';';
+            if(a[i] == 'Q') a[i] = '<';
+            if(a[i] == 'K') a[i] = '=';
+            if(a[i] == 'A') a[i] = '>';
+        }
+        for(p=0;p<12;p++){
+            for(i=0;i<12;i++) {
+            if(a[i]>a[i+1] ) {
+                b = a[i];
+                a[i] = a[i+1];
+                a[i+1] = b;
+            }
+            }
+        }
+        for(i=0;i<13;i++){
+            if(a[i] == ':') a[i] = 'T';
+            if(a[i] == ';') a[i] = 'J';
+            if(a[i] == '<') a[i] = 'Q';
+            if(a[i] == '=') a[i] = 'K';
+            if(a[i] == '>') a[i] = 'A';
+            printf("%c",a[i]);
+        }
+        return 0;
+    }

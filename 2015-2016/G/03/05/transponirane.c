9f227108343fe124c16351069ffe7e6f75622437
Atira12 <tigerpaw@abv.bg>
2015-10-24 21:41:45 +0300

    homework 5 number :3

---
+++ 2015-2016/G/03/05/transponirane.c
@@ -0,0 +1,52 @@
+ #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int mass[20][20];
+    int maxs[20][20];
+    int i=0;
+    int choice=0;
+    int b=0;
+    int dex=0;
+
+    for(i=0;i<4;i++)
+   {
+       for(choice=0;choice<4;choice++){
+            printf("\n enter number=");
+       scanf("\n%d",&mass[i][choice]);
+}
+   }
+   for(i=0;i<4;i++)
+   {
+       printf("\n");
+for(choice=0;choice<4;choice++){
+            printf("\t%d",mass[i][choice]);
+}
+   }
+   printf("\n");
+   printf("\n");
+   printf("\n");
+   printf("\n");
+   for(i=0;i<4;i++)
+   {
+       for(choice=0;choice<4;choice++){
+           maxs[choice][i]=mass[i][choice];
+}
+}
+
+
+for(i=0;i<4;i++)
+{
+
+    for(choice=0;choice<4;choice++)
+        printf("%d\t",maxs[i][choice]);
+        printf("\n");
+
+
+
+
+}
+    return 0;
+}
+

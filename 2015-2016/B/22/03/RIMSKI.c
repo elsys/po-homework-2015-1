01617c6e916e6b89d6085ca20233e43241c7304a
nikolatzotchev <ntzotchev@gmail.com>
2015-10-24 16:12:26 +0300

    oprawih zada4ata s rimskite cifri

--- 2015-2016/B/22/03/RIMSKI.c
+++ 2015-2016/B/22/03/RIMSKI.c
@@ -10,21 +10,37 @@ int main(){
         l = strlen(Swiftrage);
 
 
-        for(i=0;i<l;i++){
+        for(i=0;i<=l;i++){
 	
 		if(Swiftrage[i]=='C') Kappa=Kappa+100;
 		if(Swiftrage[i]=='L') Kappa=Kappa+50;
-		if(Swiftrage[i]=='X') Kappa=Kappa+10;
-    		if(Swiftrage[i]=='V') Kappa=Kappa+5;
+		if(Swiftrage[i]=='V') Kappa=Kappa+5;
+		if(Swiftrage[i]=='X'){
+			if(Swiftrage[i+1]=='C' || Swiftrage[i+1]=='L')
+			{ 
+				Kappa=Kappa-10;
+			
+			}else Kappa=Kappa+10; 
+		
+ 			}			
+    		
+			
 		if(Swiftrage[i]=='I'){
 			
-			if(Swiftrage[i+1]=='X') Kappa= Kappa-1;
-			if(Swiftrage[i+1]=='V') Kappa = Kappa -1;
-			if(Swiftrage[i+1]=='I'){ Kappa= Kappa+1;
-			}else if(Swiftrage[i-1]=='I') Kappa= Kappa+1;
-}
+			if(Swiftrage[i+1]=='X') Kappa = Kappa - 1;
+			if(Swiftrage[i-1]=='C' && Swiftrage[i+1] !='X' && Swiftrage[i+1] !='L' && Swiftrage[i+1] !='V') Kappa=Kappa+1;
 
-   }
+			if(Swiftrage[i-1]=='L' && Swiftrage[i+1] !='X' && Swiftrage[i+1] !='C' && Swiftrage[i+1] !='V' ) Kappa=Kappa+1;
+
+			if(Swiftrage[i+1]=='V') Kappa = Kappa - 1;
+
+			if(Swiftrage[i-1]=='X' && Swiftrage[i+1] !='X' && Swiftrage[i+1] !='V') Kappa = Kappa + 1;
+			if(Swiftrage[i-1]=='V') Kappa = Kappa + 1;
+
+			if(Swiftrage[i-1]=='I') Kappa = Kappa + 1;
+		}
+
+}
 
 printf("%d",Kappa);
 printf("\n");

e99ba10418ab2cdc1d6f4b6391b372ad3aa40881
TechyPack <vmladenov22@gmail.com>
2015-10-25 20:38:35 +0200

    Fixed cards.c & romenumbers.c

--- 2015-2016/B/08/03/cards.c
+++ 2015-2016/B/08/03/cards.c
@@ -3,17 +3,15 @@
 
 int main() 
 {
-	char cards[14];
+	char cards[13];
 	int i, l, k = 0, j, temp;
 
 	scanf("%s", cards);
 	l = strlen(cards);
-	for(i = 0; i < 14; i++)
+	for(i = 0; i < 13; i++)
 	{
-		if(cards[i] == '1' && cards[i+1] != '0'){k = 1; break;}
-		if(cards[i] == '0' && cards[i-1] != '1'){k = 1; break;}
 		
-		for(j = 0; j < 14; i++)
+		for(j = 0; j < 13; i++)
 		{
 			if(j == i) j++;
 			if(cards[j] == cards[i]){k = 1; break;}
@@ -21,96 +19,90 @@ int main()
 
 	}
 
-	if(l < 14 || l > 14){k = 1;}
+	if(l < 13 || l > 13){k = 1;}
 
 
 	if(!k)
 	{
-		for(j = 1; j < 14; j++)
-			for(i = 0; i < 14; i++)
+		for(j = 1; j < 13; j++)
+			for(i = 0; i < 13; i++)
 			{
 				if(cards[i] == 'A')
-				{
-					temp = cards[0];
-					cards[0] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '2')
-				{
-					temp = cards[1];
-					cards[1] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '3')
-				{
-					temp = cards[2];
-					cards[2] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '4')
-				{
-					temp = cards[3];
-					cards[3] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '5')
-				{
-					temp = cards[4];
-					cards[4] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '6')
-				{
-					temp = cards[5];
-					cards[5] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '7')
-				{
-					temp = cards[6];
-					cards[6] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '8')
-				{
-					temp = cards[7];
-					cards[7] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '9')
-				{
-					temp = cards[8];
-					cards[8] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '1')
-				{
-					temp = cards[9];
-					cards[9] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == '0')
-				{
-					temp = cards[10];
-					cards[10] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == 'J')
-				{
-					temp = cards[11];
-					cards[11] = cards[i];
-					cards[i] = temp;
-				}
-				if(cards[i] == 'Q')
 				{
 					temp = cards[12];
 					cards[12] = cards[i];
 					cards[i] = temp;
 				}
+				if(cards[i] == '2')
+				{
+					temp = cards[0];
+					cards[0] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '3')
+				{
+					temp = cards[1];
+					cards[1] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '4')
+				{
+					temp = cards[2];
+					cards[2] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '5')
+				{
+					temp = cards[3];
+					cards[3] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '6')
+				{
+					temp = cards[4];
+					cards[4] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '7')
+				{
+					temp = cards[5];
+					cards[5] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '8')
+				{
+					temp = cards[6];
+					cards[6] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == '9')
+				{
+					temp = cards[7];
+					cards[7] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == 'T')
+				{
+					temp = cards[8];
+					cards[8] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == 'J')
+				{
+					temp = cards[9];
+					cards[9] = cards[i];
+					cards[i] = temp;
+				}
+				if(cards[i] == 'Q')
+				{
+					temp = cards[10];
+					cards[10] = cards[i];
+					cards[i] = temp;
+				}
 				if(cards[i] == 'K')
 				{
-					temp = cards[13];
-					cards[13] = cards[i];
+					temp = cards[11];
+					cards[11] = cards[i];
 					cards[i] = temp;
 				}
 			}
--- 2015-2016/B/08/03/romenumbers.c
+++ 2015-2016/B/08/03/romenumbers.c
@@ -14,24 +14,20 @@ int main()
 	for(i = 0; i < l; i++)
 	{
 		if(num[i] == 'C' && num[i-1] != 'I') sum += 100;
-		if(num[i] == 'C' && num[i-1] == 'I' && num[i-2] !='I') sum += 99;
-		if(num[i] == 'C' && num[i-1] == 'I' && num[i-2] == 'I') sum += 98;
+		if(num[i] == 'C' && num[i-1] == 'I') sum += 99;
 
 		if(num[i] == 'L' && num[i-1] != 'I') sum += 50;
-		if(num[i] == 'L' && num[i-1] == 'I' && num[i-2] !='I') sum += 49;
-		if(num[i] == 'L' && num[i-1] == 'I' && num[i-2] == 'I') sum += 48;
+		if(num[i] == 'L' && num[i-1] == 'I') sum += 49;
 
 		if(num[i] == 'X' && num[i-1] != 'I') sum += 10;
-		if(num[i] == 'X' && num[i-1] == 'I' && num[i-2] !='I') sum += 9;
-		if(num[i] == 'X' && num[i-1] == 'I' && num[i-2] == 'I') sum += 8;
+		if(num[i] == 'X' && num[i-1] == 'I') sum += 9;
 
 		if(num[i] == 'V' && num[i-1] != 'I') sum += 5;
-		if(num[i] == 'V' && num[i-1] == 'I' && num[i-2] !='I') sum += 4;
-		if(num[i] == 'V' && num[i-1] == 'I' && num[i-2] == 'I') sum += 3;
+		if(num[i] == 'V' && num[i-1] == 'I') sum += 4;
 
-		if(num[i] == 'I' && num[i+1] == 0) sum += 1;
-		if(num[i] == 'I' && num[i+1] == 'I' && num[2] == 0) sum += 2;
-		if(num[i] == 'I' && num[i+1] == 'I' && num[3] == 'I' && num[4] == 0) sum += 3; 
+		if(num[i] == 'I' && num[i-1] != 'I' && num[i+1] == 0) sum += 1;
+		if(num[i] == 'I' && num[i-1] != 'I' && num[i+1] == 'I' && num[i+2] == 0) sum += 2;
+		if(num[i] == 'I' && num[i-1] != 'I' && num[i+1] == 'I' && num[i+2] == 'I' && num[i+3] == 0) sum += 3; 
 
 	}
 

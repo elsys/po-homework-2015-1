a74c6541143bda552404588d2d44d588181d1e26
Ivan3fonov <ivan3fonov@gmail.com>
2015-10-26 17:50:30 +0200

    Update STOP.c
    Оправена задача от домашно 03 !

--- 2015-2016/B/17/02/STOP.c
+++ 2015-2016/B/17/02/STOP.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
     int  main()
 {
     char s[100];
     int i,j;
-   for(i=0;i<99;i++)
-    scanf("%[^\n]s",&s);
+
+    scanf("%[^'STOP']s",s);
      j=strlen(s);
 
-    for(i=0;i<j;i++)
+    for(i=0;i<j;i++)O
     {
-    if(s[i]=='s' && s[i+1]=='t' && s[i+2]=='o' && s[i+3]=='p') break;
 
     if(s[i]>='a' && s[i]<='z') s[i]=s[i]-32;
 

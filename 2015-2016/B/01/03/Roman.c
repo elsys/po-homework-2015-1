8c11659995021c2f6d7c8006872070745ec472ec
Verbo1806 <sashoverbovskiy@gmail.com>
2015-10-26 18:08:34 +0200

    Домашно 3
    Alexander Verbovskiy/B/01

---
+++ 2015-2016/B/01/03/Roman.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i=0,x,l,m,ca[10],k=0;
+    char c[10];
+
+        printf("Vavedi chislo - ");scanf("%s", c);
+        m=strlen(c);
+
+        while(i!=m){
+        if(c[i]>='I') ca[i]=1;
+        if(c[i]=='V') ca[i]=5;
+        if(c[i]=='X') ca[i]=10;
+        if(c[i]=='L') ca[i]=50;
+        if(c[i]=='C') ca[i]=100;
+         i++;}
+        i=0;
+
+        while(i!=m-1){
+        if(ca[i]<ca[i+1]) k=k+(ca[i+1]-ca[i]);
+        if(ca[i]>ca[i+1] || ca[i]==ca[i+1] )if(i!=m-2){k=k+ca[i];}
+        else k=k+ca[i]+ca[i+1];
+            i++;
+        }
+        printf("\n%d",k);
+
+    return 0;
+}

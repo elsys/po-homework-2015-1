d207afcbc8db2a69c23922aa53355a3292f29185
Slavkata <slavkirilov00@gmail.com>
2015-10-26 12:04:46 +0200

    reverse Update

--- 2015-2016/B/26/03/reverse.c
+++ 2015-2016/B/26/03/reverse.c
@@ -2,13 +2,15 @@
 #include<string.h>
 
 int main(){
-	int b;
 	char s[50];
-	scanf("%s",&s[50]);
+	char b;
+	fgets(s,50,stdin);
 	b=strlen(s);
+	b--;
 	while(b>=0){
 		printf("%c",s[b]);
 		b--;
 	}
+	printf("\n");
 return 0;
 }

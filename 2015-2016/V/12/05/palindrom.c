ecab70c820dddfc2057b99557bfe62d231dead7a
Ivan <ivanmilevtues@gmail.com>
2015-10-27 21:14:55 +0200

    Palindrom.c вече е с функция :)
    Направих програмата с функцция както си е и по условие.

--- 2015-2016/V/12/05/palindrom.c
+++ 2015-2016/V/12/05/palindrom.c
@@ -2,20 +2,26 @@
 #include <string.h>
 #define max 128
 
-
+int check(int len, char palindromlisi[]);
 
 int main(){
 	char palindromlisi[max];
 	scanf("%s",palindromlisi);
 	int len = strlen(palindromlisi);
+	if(check(len,palindromlisi)){
+		printf("Yes it is.\n");
+	}
+	else
+		printf("No it is.\n");
+	
+}
+int check(int len, char palindromlisi[]){
 	int i;
 	for(i = 0;i<len;i++,len--)
 	{
 		if(palindromlisi[i] != palindromlisi[len-1]){
-			printf("No it isn't\n");
 			return 0;
 		}
 	}
-	printf("Yes it is.\n");
 	return 1;
 }

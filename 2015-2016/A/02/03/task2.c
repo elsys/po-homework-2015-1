169a61a7bf12d96c456f7644d7169f322ed9cf0d
vincho7012 <mihailovblagovest@gmail.com>
2015-10-27 10:43:07 +0200

    Homework03

--- 2015-2016/A/02/03/task2.c
+++ 2015-2016/A/02/03/task2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
-{   int i;
+{   int i,n;
     char s[21];
     fgets(s,21,stdin);
-    for(i=19;i>=0;i--)
+    n=strlen(s);
+    for(i=n;i>=0;i--)
         printf("%c",s[i]);
     return 0;
 }

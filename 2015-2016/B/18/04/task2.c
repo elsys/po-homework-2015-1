7949d1c74f5b5c021f512411045c7bd1ec5b1da2
LMladenov <lachomladenov@gmail.com>
2015-11-01 20:04:58 +0200

    Homework03 - Lachezar Mladenov 10B No.18

---
+++ 2015-2016/B/18/04/task2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int exact_square(int);
+
+int main()
+{
+	int number = 0;
+
+	scanf("%d", &number);
+
+	exact_square(number);
+
+	printf("%d\n", exact_square(number));
+
+	return 0;
+}
+
+int exact_square(int number)
+{
+	int count = 0;
+	int value = 0;
+
+	for(count = 0; count <= number / 2; count++)
+	{
+	    if(sqrt(number) == count)
+	    {
+		    value = 1;
+	    }
+
+	}
+
+	return value;
+
+}

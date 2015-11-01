b9cfa583bbf93369cdfb05e218b86b4480ecac91
Verbo1806 <sashoverbovskiy@gmail.com>
2015-11-01 18:01:38 +0200

    Домашно 4
    Александър Вербовский , 10 Б , №1

---
+++ 2015-2016/B/01/04/task2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int exact_square(int sq){
+
+float counter;
+
+ counter=sq;
+
+    while(counter!=0)
+    {
+        if(sq/counter==counter){
+        printf("1");
+        break;}
+
+        counter--;
+    }
+
+ if(counter==0) printf("0");
+
+ return 0;
+}
+
+int main(){
+
+int sq;
+
+ scanf("%d",&sq);
+
+ exact_square(sq);
+
+    return 0;
+}

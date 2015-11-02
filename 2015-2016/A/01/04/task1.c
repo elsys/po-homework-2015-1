5c984fa919c6062556eb5b06343aa3a163f57b53
Yellobelly <alexstoyanof@gmail.com>
2015-11-02 21:52:06 +0200

    task1 raboteshta

--- 2015-2016/A/01/04/task1.c
+++ 2015-2016/A/01/04/task1.c
@@ -2,15 +2,14 @@
 
 int is_prime (int n,int i)
 	{
-	for(i=2;i<=n/2;i++)
+	for(i=2;i<=n;i++)
 		{
-		if (n%i==0)
+		if (n%i==0 && i!=n)
 			{
 			return 0;			
 			}
-		else return 1;
 		}	
-
+	return 1;	
 	}
 int main ()
 	{

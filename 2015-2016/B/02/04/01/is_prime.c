9bb1f6c1dc535e108cfdbef6e0395ee19f614444
Andrey Strinski <Kroxitrock@gmail.com>
2015-11-03 20:48:47 +0200

    Домашно 04
    Домашно на Андрей Стрински от 9б кл No2

--- 2015-2016/B/02/04/01/is_prime.c
+++ 2015-2016/B/02/04/01/is_prime.c
@@ -1,38 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 
-int is_prime(char* input_numb)
+int is_prime(int numb)
 {
-	int  lenght,numb=0, counter;
+	int counter, true_false=0;
 
 
-	lenght = strlen( input_numb );
-   	for ( counter = 0; counter < lenght; counter++)
-        	if ( input_numb[ counter ] >= '0' && input_numb[ counter ] <= '9') numb = numb + input_numb[ counter ] ;
-
-
-
-	if ( numb < 1 ) return -1;
-	else
-	{
-		for ( counter = numb - 1; counter > 3; counter-- )
-		{
-			if( numb % counter == 0 ) return 0;
-		}
-	}
-
-	return 1;
+	
+	for ( counter = numb - 1; counter > 2; counter-- )
+		if( numb % counter == 0 ) true_false++;
+	if(numb < 1) return -1;
+	else if(true_false > 0) return 0;
+	else return 1;
 }
-
 
 
 int main()
 {
-	char number[50];
+	int number;
 
 
 
-	scanf ( "%s", number );
+	scanf ( "%d", &number );
 	printf( "%d", is_prime(number) );
 
 

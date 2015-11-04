2fb97733b71dc188c396977d003648bfe17af2b2
Denis <denkatapetrow@gmail.com>
2015-11-04 21:20:57 +0200

    Task1.c

---
+++ 2015-2016/G/10/06/task1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int is_prime(int n);
+
+int main(){
+
+	int n;
+
+	if(!scanf("%d", &n)){
+
+		printf("%d \n", -1);
+
+	}else{
+
+		printf("%d \n", is_prime(number));
+
+	}
+
+	return 0;
+
+}
+
+int is_prime(int n){
+
+	int i;
+
+	for(i = 2;i < n/2;i++){
+
+		if(n%i == 0){
+
+			return 0;
+
+		}
+
+	}
+
+	return 1;
+
+}

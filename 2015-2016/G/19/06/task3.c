207f14c22b872d606d1dd085e9979111f75c10d3
iliancho99 <iliqn.kordev@gmail.com>
2015-11-04 20:52:57 +0200

    Iliyan Kordev №19 6 homework

--- 2015-2016/G/19/06/task3.c
+++ 2015-2016/G/19/06/task3.c
@@ -1,25 +1,85 @@
 #include <stdio.h>
 #include <string.h>
-int is_prime(int n);
+
+void create_letter_distribution_diagram();
+
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    printf("%d", exact_square(n));
+    create_letter_distribution_diagram();
     return 0;
 }
 
-int exact_square(int n)
+void create_letter_distribution_diagram()
 {
+    FILE *fp;
+    long lSize;
+    char *buffer;
 
-    int a;
-    for(a = 0; a <= n; a++)
+    fp = fopen ( "in.txt" , "rb" );
+    if( !fp ) return;
+
+    fseek( fp , 0L , SEEK_END);
+    lSize = ftell( fp );
+    rewind( fp );
+
+    buffer = calloc( 1, lSize+1 );
+    if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
+
+    if( 1!=fread( buffer , lSize, 1 , fp) )
+      fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
+
+    int data[26][2];
+    int i;
+    for(i = 0; i < 26;i++){
+        data[i][0] = i;
+        data[i][1] = 0;
+    }
+
+    for(i = 0; buffer[i]; i++)
     {
-        if (n == a * a)
+        char c = tolower(buffer[i]);
+        if(c >= 'a' && c <= 'z')
         {
-            return 1;
+            data[c - 'a'][1]++;
+        }
+    }
+    int c, d;
+    int swap[2];
+    int length = strlen(buffer);
+    for (c = 0 ; c < ( length - 1 ); c++)
+    {
+        for (d = 0 ; d < length - c - 1; d++)
+        {
+            if (data[d][1] < data[d+1][1])
+            {
+                swap[0] = data[d][0];
+                swap[1] = data[d][1];
+                data[d][0] = data[d+1][0];
+                data[d][1] = data[d+1][1];
+                data[d+1][0] = swap[0];
+                data[d+1][1] = swap[1];
+            }
+            else if(data[d][1] == data[d+1][1])
+            {
+                if(data[d][0] > data[d+1][0])
+                {
+                    swap[0] = data[d][0];
+                    swap[1] = data[d][1];
+                    data[d][0] = data[d+1][0];
+                    data[d][1] = data[d+1][1];
+                    data[d+1][0] = swap[0];
+                    data[d+1][1] = swap[1];
+                }
+            }
         }
     }
 
-    return 0;
+    for(i = 0; i < 27;i++)
+    {
+        printf("%c: %d\n", data[i][0] + 'a', data[i][1]);
+    }
+
+    fclose(fp);
+    free(buffer);
 }
+

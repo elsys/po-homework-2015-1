3f42584413f0651fe9f8f2629b6e7a117f6ee6a9
Boris Mutafov <mutafow@gmail.com>
2015-11-06 00:53:44 +0200

    create task1.c

---
+++ 2015-2016/B/03/05/task1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c, rotate_times;
+    scanf( "%d %d %d %d" , &a , &b , &c , &rotate_times );
+    for ( ; rotate_times > 0 ; rotate_times-- ) {
+        rotr( &a , &b, &c );
+    }
+    printf("%d %d %d", a , b , c);
+    return 0;
+}
+
+void rotr ( int *a , int *b , int *c ) {
+    int swap_help;
+    swap_help = *a;
+    *a = *c;
+    *c = *b;
+    *b = swap_help;
+}

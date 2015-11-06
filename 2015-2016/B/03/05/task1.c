e3f5ff7fafb09b40e1ce7368457465735da636b5
Boris Mutafov <mutafow@gmail.com>
2015-11-06 17:57:51 +0200

    премахнати warnings на linux на първата задача

--- 2015-2016/B/03/05/task1.c
+++ 2015-2016/B/03/05/task1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void rotr ( int * , int * , int * );
+
 int main() {
     int a, b, c, rotate_times;
     scanf( "%d %d %d %d" , &a , &b , &c , &rotate_times );
     for ( ; rotate_times > 0 ; rotate_times-- ) {
-        rotr( &a , &b, &c );
+        rotr( &a , &b , &c );
     }
     printf("%d %d %d", a , b , c);
     return 0;

8b936dd82eda9176d4a1d10c946a779393c9fe64
nikolatzotchev <ntzotchev@gmail.com>
2015-11-06 19:29:35 +0200

    task1.c

---
+++ 2015-2016/B/22/05/task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int rotate;
+void rotr ( int *a, int *b, int *c );
+int main(){
+	int a,b,c;
+	scanf("%d %d %d %d",&a ,&b ,&c ,&rotate);
+	rotr(&a , &b , &c);
+	printf("%d, %d, %d", a ,b ,c);
+
+ return 0;
+}
+void rotr ( int *a, int *b, int *c ) {
+	int helper, helper1; 	
+	for(helper = 0; helper < rotate; helper++){
+		helper1 = *a;
+		*a = *c;
+		*c = *b;
+		*b = helper1;
+			
+ }		
+
+}

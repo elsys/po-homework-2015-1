15339a86578f4bb85ac12afc1ee7d1dccd22c71e
VelislavYotov <slaviy999@abv.bg>
2015-11-07 18:32:32 +0200

    Update task1.c

--- 2015-2016/A/05/04/task1.c
+++ 2015-2016/A/05/04/task1.c
@@ -4,51 +4,54 @@
 /*Velislav Yotov 10a Nomer-5
 task1-is_prime function
 Proverqva dali chisloto e prosto.Ako e prosto:1 ako ne:0 ako ima greshka:-1*/
-int str_into_int(char* string)
+int str_into_int(char* input)
 {
-int  i, len;
-int result=0;
-len = strlen(string);
-for(i=0; i<len; i++)
+	int i,number,length;
+	length=strlen(input)-1;
+	for(i=0,number=0;i<length;i++)
+     {
+		number+=input[i]-'0';
+		 if(i!=length-1)
+            {
+                number*=10;
+            }
+	 }
+	 return number;
+}
+
+int is_prime(char string[10])
+{
+    int number,i,len;
+	len =strlen(string)-1;
+	for(i=0; i<len; i++)
     {
-        result=result*10+(string[i]-'0');
+		if(string[i]<'0' || string[i]>'9')
+        {
+			printf("-1");
+			return -1;
+		}
+	}
+    number=str_into_int(string);
+    if(number==0 || number==1)
+    {
+    return 0;
+    printf("0");
     }
-is_prime(result);
+    for(i=2;i<number;i++){
+		if(number%i==0)
+        {
+            printf("0");
+            return 0;
+        }
+	}
+	return 1;
+	printf("1")
 }
-int is_prime(result)
+
+int main(void)
 {
-    int counter,l=0,number;
-    number=result;
-    for(counter=1;counter<=number;counter++)
-        {
-            if(number%counter==0)
-                {
-                    l++;
-                }
-        }
-  if (l==2)
-      printf("1");
-  else
-      printf("0");
-    return 0;
-}
-
-int main()
-{   int lenght,i,m=0;
     char string[10];
-    scanf("%s",string);
-    lenght=strlen(string);
-    for(i=0;i<lenght;i++)
-        {
-            if(string[i]>='0' && string[i]<='9')
-                m++;
-        }
-    if(m==lenght)
-        str_into_int(string);
-    else{
-        printf("-1");
-    }
+    fgets(string,10,stdin);
+    is_prime(string);
     return 0;
 }
-
-

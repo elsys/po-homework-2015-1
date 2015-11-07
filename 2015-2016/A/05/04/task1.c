5e0275d75a39382b30caa40e4c08edb729fd02bd
VelislavYotov <slaviy999@abv.bg>
2015-11-07 17:00:35 +0200

    Update task1.c

--- 2015-2016/A/05/04/task1.c
+++ 2015-2016/A/05/04/task1.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Velislav Yotov 10a Nomer-5
-//task1-is_prime function
-//Proverqva dali chisloto e prosto.Ako e prosto:1 ako ne:0 ako ima greshka:-1
+/*Velislav Yotov 10a Nomer-5
+task1-is_prime function
+Proverqva dali chisloto e prosto.Ako e prosto:1 ako ne:0 ako ima greshka:-1*/
+int str_into_int(char* string)
+{
+int  i, len;
+int result=0;
+len = strlen(string);
+for(i=0; i<len; i++)
+    {
+        result=result*10+(string[i]-'0');
+    }
+is_prime(result);
+}
 int is_prime(result)
 {
     int counter,l=0,number;
@@ -39,15 +50,5 @@ int main()
     }
     return 0;
 }
-int str_into_int(char* string)
-{
-int  i, len;
-int result=0;
-len = strlen(string);
-for(i=0; i<len; i++)
-    {
-        result=result*10+(string[i]-'0');
-    }
-is_prime(result);
-}
+
 

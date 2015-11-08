e4bdcbc39cfa8bcfd2c7676af753d71aeb71d971
Boris Mutafov <mutafow@gmail.com>
2015-11-08 13:52:01 +0200

    fixed when first and last number are 1 - must be 00

--- 2015-2016/B/03/05/task2.c
+++ 2015-2016/B/03/05/task2.c
@@ -6,40 +6,43 @@ int repeated = 0;
 void next_generation( int * , int * , int );
 
 int main() {
-    int current[ 22 ];
-    int next[ 22 ];
-    int lenght , lenght_helper , cell_current , i;
-    for ( i = 0 ; i < 22 ; i++ ) {
+    int current[ 20 ];
+    int next[ 20 ];
+    int lenght , lenght_helper , cell_current , i , error = 0;
+    for ( i = 0 ; i < 20 ; i++ ) {
         *( current + i ) = 0;
         *( next + i ) = 0;
     }
     do {
         scanf( "%d" , &lenght );
     } while( lenght > 20 || lenght < 0 );
-    for ( lenght_helper = lenght , cell_current = 1 ; lenght_helper > 0 ; lenght_helper -- , cell_current ++ ) {
+    for ( lenght_helper = lenght , cell_current = 0 ; lenght_helper > 0 ; lenght_helper -- , cell_current ++ ) {
         scanf ( "%d" , &current[ cell_current ] );
+        if ( cell_current == 0 || cell_current == lenght - 1) {
+            if ( current [ cell_current ] == 1 ) {
+                printf ( " Error : String must start and end with 0. ");
+                error = 1;
+                break;
+            }
+        }
     }
-    next_generation( current , next , lenght );
+    if ( error == 0 ) next_generation( current , next , lenght );
     return 0;
 }
 
 void next_generation( int *current , int *next , int lenght ) {
     int i , alive = 0 , start, end;
-    for ( i = 0 ; i <= lenght ; i++ ) {
+    for ( i = 0 ; i <= lenght; i++ ) {
         if( *( current + i ) == 1 ) {
                 alive ++ ;
         }
     }
-    for ( i = 1 ; i <= lenght ; i++ ) {
+    for ( i = 0 ; i < lenght ; i++ ) {
         if( *( current + i ) == 0 ) printf( " ." );
         else printf( " *" );
     }
-    if ( *(current + 1) == 0 ) start = 2;
-    else start = 1;
-    if ( *(current + lenght ) == 0 ) end = lenght - 1;
-    else end = lenght;
     if( alive > 0 && repeated < 1000 ) {
-        for ( i = start ; i <= end ; i++ ) {
+        for ( i = 1 ; i < lenght - 1 ; i++ ) {
             if ( *( current + i + 1 ) == *( current + i - 1 ) ) {
                 *( next + i ) = 0;
             } else {

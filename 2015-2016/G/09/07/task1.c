4d2fa5725099efc18b5c3fe6a40a67fe1ed756a5
David-Nicollas <david.nicolas06.99@Gmail.com>
2015-11-10 00:01:31 +0200

    task1.c

--- 2015-2016/G/09/07/task1.c
+++ 2015-2016/G/09/07/task1.c
@@ -1,29 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void rotr (int *a, int *b, int *c)
-{
-int temp;
-temp = *c;
-*c = *b;
-*b = *a;
-*a = temp;
-printf("%d %d %d\n",*a,*b,*c);
+void rotr(int *a, int *b, int *c){
+    int swapper1, swapper2, swapper3;
+        swapper1=*a;
+        swapper2=*b;
+        swapper3=*c;
+        *a=swapper3;
+        *b=swapper1;
+        *c=swapper2;
 }
-int main() {
-	
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int n = 0;
-	int i;
-	scanf("%d", &a);
-	scanf("%d", &b);
-	scanf("%d", &c);
-	scanf("%d", &n);
-	for (i= 0; i<n; i++)
-	{
-		rotr(&a,&b,&c);
-		
-	}
-	return 0;
-}
+
+int main(){
+        
+    int a, b, c, n, count;
+    printf("Enter numbers and number of rotations\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &n);
+    for(count=0; count<n; count++){
+        rotr(&a, &b, &c);
+    }
+    printf("\n %d %d %d \n", a, b, c);    

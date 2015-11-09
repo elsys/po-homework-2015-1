3706949b6092004050331e52ca21bcc034430ee0
PetkoMihaylov <pet99@mail.bg>
2015-11-09 18:22:14 +0200

    Петко Михайлов 10а №23 homework04.

---
+++ 2015-2016/A/23/05/task1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+void rotr(int *a,int *b,int *c)
+{
+    int num;
+     num = *c;
+    *c = *b;
+    *b = *a;
+    *a = num;
+}
+int main()
+{
+    int a,b,c,n,i=0;
+    printf("a=");scanf("%d",&a);
+    printf("b=");scanf("%d",&b);
+    printf("c=");scanf("%d",&c);
+    printf("n=");scanf("%d",&n);
+    int *var1;
+    int *var2;
+    int *var3;
+    var1 = &a;
+    var2 = &b;
+    var3 = &c;
+    while (i<n)
+    {
+        i++;
+        rotr(var1,var2,var3);
+    }
+    printf("%d ",*var1);
+    printf("%d ",*var2);
+    printf("%d",*var3);
+    printf("\n");
+    return 0;
+}

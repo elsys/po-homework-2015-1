77542c84ad8b52c4de7413dc3f77a58390fa33c4
doroteq <doroteqpetrova1@gmail.com>
2015-11-10 21:45:45 +0200

    task2

---
+++ 2015-2016/G/13/07/task2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+void next_generation(int*, int*, int);
+
+int main()
+{
+    int n, i;
+    int hasAnotherLoop = 1;
+    int current[20];
+    int next[20];
+    int loops;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &current[i]);
+    }
+
+    do
+    {
+        hasAnotherLoop = 0;
+        for(i = 0; i < n; i++)
+        {
+            if(current[i] == 1)
+            {
+                hasAnotherLoop = 1;
+                break;
+            }
+        }
+
+        for(i = 0; i < n; i++)
+            {
+                if(current[i] == 1){
+                    printf("*");
+                } else {
+                    printf(".");
+                }
+            }
+
+        printf("\n");
+
+        next_generation(current, next, n);
+        for(i = 0; i < n; i++){
+            current[i] = next[i];
+        }
+        loops++;
+    }while(hasAnotherLoop == 1 && loops < 1000);
+
+    return 0;
+}
+
+void next_generation(int *current, int *next, int length)
+{
+    int i;
+    next[0] = current[0];
+    next[length - 1] = current[length - 1];
+    for(i = 1; i < length - 1; i++)
+    {
+        if((current[i-1] == 1 && current[i + 1] == 1)||
+           (current[i-1] == 0 && current[i + 1] == 0)) {
+            next[i] = 0;
+        } else {
+             next[i] = 1;
+        }
+    }
+}

4e18f824b4ee025fd8304a466b0b013c7ce91753
ivaylodimitrovtues <ivaylodimitrovtues@gmail.com>
2015-11-10 20:54:18 +0200

    Домашно номер-7
    Ивайло Димитров Димитров 10в клас No-11
    Присъства първа задача от седмо домашно.

---
+++ 2015-2016/V/11/07/task1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void rotr(int *a,int *b,int *c);
+void rotr(int *a,int *b,int *c)
+{
+    int k=*a;
+    *a=*c;
+    *c=*b;
+    *b=k;
+      }
+int main()
+{
+    int a,b,c,times;
+    int i=0;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&times);
+    do{
+        rotr(&a,&b,&c);
+        i++;
+    }while(i<times);
+
+    printf("%d %d %d",a,b,c);
+    return 0;
+}

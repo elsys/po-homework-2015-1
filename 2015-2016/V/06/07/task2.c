2da4452e1b540b58933fe535642cc3d927bd62c4
bojan-dim <bojan_dim@abv.bg>
2015-11-11 23:51:52 +0200

    task2.c Боян Димитров 10в номер 6 домашно 7
    Fixed some bugs and warnings

--- 2015-2016/V/06/07/task2.c
+++ 2015-2016/V/06/07/task2.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-void next_generation(int *, int *, int);
+void next_generation(int*, int* , int);
 
 int main(){
-	int lenght, i=0;
+	int n, i=0;
 	int current[20];
 	int next[20];
-	scanf("%d", &lenght);
-	for(i=0;i<lenght;i++){
-		scanf("%d ", &current[i]);
+	scanf("%d", &n);
+	for(i=0;i<n;i++){
+		scanf("%d", &current[i]);
 	}
-	int alive=0;
+	int alive=1;
 	do{
 		alive=0;
-		for(i=0;i<lenght;i++){
+		for(i=0;i<n;i++){
 			if(current[i]==1){
 				alive=1;
 				break;
 			}
-			
 		}
-		for(i=0;i<lenght;i++){
+		for(i=0;i<n;i++){
 			if(current[i]==0){
 				printf(".");
 			}
@@ -27,27 +26,25 @@ int main(){
 			if(current[i]==1){
 				printf("*");		
 			}
-			
+
 		}
 		printf("\n");
-		next_generation(current, next, length);
-		for(i=0;i<lenght;i++){
+		next_generation(current, next, n);
+		for(i=0;i<n;i++){
 			current[i]=next[i];//Zapisvame nomera na vsiaka kletka;
 		}
 	}while(alive==1);
-
+	return 0;
 }
 
 void next_generation(int *current, int *next, int length){
 	int i; 
-	current[0]=next[0];
-	next[length-1]=current[lenght-1];
+	next[0]=current[0];
+	next[length-1]=current[length-1];
 	for(i=0;i<length-1;i++){
-		if(current[i-1]==1 && current[i+1]==1){
-			if(current[i-1]==0 && current[i+1]==0{
+		if((current[i-1]==1 && current[i+1]==1)||
+		  (current[i-1]==0 && current[i+1]==0)){
 				next[i]=0;
-			}
-		
 		}
 		else{
 			next[i]=1;

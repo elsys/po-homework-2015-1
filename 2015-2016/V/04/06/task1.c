6114e6e399560bb655e9582abee29f703b841782
tonykolev <tonykolev150@gmail.com>
2015-11-23 22:16:34 +0200

    HW 06, task1&task2

---
+++ 2015-2016/V/04/06/task1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int is_prime(int num);
+
+int main()
+{
+	int num;
+
+	printf("num=");
+	scanf("%d", &num);
+
+	printf("%d\n", is_prime(num));
+}
+
+
+int is_prime(int num){
+	int div=2;
+	
+	while(num%div!=0)
+	{
+		div++;
+	}
+	if(num>=2)
+	{
+
+		if(div==num)
+			return 1;
+		if(div!=num)
+			return 0;
+	}
+	else return -1;
+	return 0;
+}
+

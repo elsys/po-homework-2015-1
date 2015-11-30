de1a49e0a3b3cb539cb0465b95c42f5c35df99a3
evgeniq <evgeniq.grigorova99@gmail.com>
2015-11-30 21:53:37 +0200

    Evgeniq Grigorova N14 10g
    new homework - 6 - task3

---
+++ 2015-2016/G/14/new_homeworks/06/task3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main () 
+{
+    char c;
+    int number = 0;
+    int alphabet[26];
+    int i = 0;
+    for(i = 0; i < 26; i++)
+    {
+        alphabet[i] = 0;
+    }
+    while (1) 
+    {
+        c = getchar();            
+        if (c == EOF) 
+        { 
+            break; 
+        }
+        putchar(c);
+        number = c - 'a';
+        alphabet[number] += 1;
+    }
+    int char_number = 0;
+    int i2 = 0;
+    int biggest_num = -1;
+    int biggest_i = -1;
+    
+    for(i = 0; i < 26; i++)
+    {
+        for(i2 = 0; i2 < 26; i2++)
+        {
+            if(alphabet[i2] > biggest_num)
+            {
+                biggest_num = alphabet[i2];
+                biggest_i = i2;
+            }
+        }
+        char_number += biggest_i + 'a';
+        printf("%c: %d\n", char_number, biggest_num);
+        alphabet[biggest_i] = -1;
+        biggest_i = -1;
+        biggest_num = -1;
+        char_number = 0;
+    }
+    return 0;
+}

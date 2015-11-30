053f1902359220f5e937e87f147e72772e329afe
evgeniq <evgeniq.grigorova99@gmail.com>
2015-11-30 21:51:52 +0200

    Evgeniq Grigorova N14 10g
    new homework - 6 - task1

---
+++ 2015-2016/G/14/new_homeworks/06/task1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int is_prime(num){
+    //printf("\n%d\n", num);
+
+    int counter = 0;
+    int check = 0;
+
+    for(counter = 2 ; counter < num ; counter++){
+        if(num % counter == 0){
+            check = 1;
+        }
+    }
+
+    if(check == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+
+}
+
+int main(){
+
+    int number;
+    int result;
+
+    scanf("%d",&number);
+
+    result = is_prime(number);
+
+    printf("\n%d\n",result);
+
+    return 0;
+
+}

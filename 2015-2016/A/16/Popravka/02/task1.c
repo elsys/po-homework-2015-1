2ce3ee13aacd9f900f8bb21ecc9ebfb39759912a
iordanivanov <iiordan.ivanov@gmail.com>
2015-12-06 18:00:18 +0200

    Create task1.c

---
+++ 2015-2016/A/16/Popravka/02/task1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+   int i = 0;
+   char ch;
+   for (i = 0; i < 256; i++)
+   {
+      printf("%c ", ch);
+      ch = ch + 1;
+   }
+   return 0l
+}

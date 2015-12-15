f81d9a265d9facf83b7f0fa935866d7e26bab636
DeyanHristov <Deyan.Xristov@gmail.com>
2015-12-15 23:14:45 +0200

    Update task1.c

--- 2015-2016/V/09/06/task1.c
+++ 2015-2016/V/09/06/task1.c
@@ -9,10 +9,10 @@ int main() {
 
     prime = is_prime(n);
     if (prime == 1) {
-      printf("1", n);
+      printf("1");
     }
    else {
-      printf("0", n);
+      printf("0");
    }
 
     return 0;

164767fcaba6dd745020c9d5d57ed659eb29147f
nikolatzotchev <ntzotchev@gmail.com>
2016-01-10 11:58:28 -0800

    task1,2,3;

---
+++ 2015-2016/B/22/06/task3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+
+int main (){
+	int number, size, count[50] , reckon ;
+	scanf("%d", &size);
+	
+	for(number = 0 ; number < size ; number ++){
+	scanf("%d", &count[number]);
+
+}
+	for(number = 0 ; number < size ; number ++){
+		for(reckon = 0 ; reckon < size ; reckon ++ ){
+			if(count[number] == count[reckon] && number != reckon){
+			 
+			 break;	
+			}
+		if(reckon + 1 == size){printf("\n\n\n%d",count[number]);}
+		
+		}
+
+
+}
+
+ printf("\n");
+
+ return 0;
+}

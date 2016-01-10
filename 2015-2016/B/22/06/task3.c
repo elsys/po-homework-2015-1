05be58ad28c6bbb5a1bb23a66f1d86039f28a7c9
nikolatzotchev <ntzotchev@gmail.com>
2016-01-10 12:14:41 -0800

    podredih task3.c

--- 2015-2016/B/22/06/task3.c
+++ 2015-2016/B/22/06/task3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
 
 int main (){
 	int number, size, count[50] , reckon ;
@@ -9,22 +8,17 @@ int main (){
 	
 	for(number = 0 ; number < size ; number ++){
 	scanf("%d", &count[number]);
-
-}
-	for(number = 0 ; number < size ; number ++){
-		for(reckon = 0 ; reckon < size ; reckon ++ ){
-			if(count[number] == count[reckon] && number != reckon){
-			 
-			 break;	
+	}
+		for(number = 0 ; number < size ; number ++){
+			for(reckon = 0 ; reckon < size ; reckon ++ ){
+				if(count[number] == count[reckon] && number != reckon){
+				 	break;	
+				}
+					if(reckon + 1 == size){	
+						printf("\n\n\n%d",count[number]);
+					}
 			}
-		if(reckon + 1 == size){printf("\n\n\n%d",count[number]);}
-		
-		}
-
-
-}
-
- printf("\n");
-
- return 0;
+		}	
+	printf("\n");
+	return 0;
 }

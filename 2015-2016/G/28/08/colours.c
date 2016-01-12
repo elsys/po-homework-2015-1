ac87888e06ab9e54ee892d70ded2f86aca8d821f
Philip Pepegov <you@example.com>
2016-01-12 23:29:59 +0200

    Голямо домашно (08) Филип Пепегов 10г клас, №28

---
+++ 2015-2016/G/28/08/colours.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	char hex_des;
+	
+	scanf("%c", &hex_des);
+	//printf("%c", hex_des);
+
+	if (hex_des == 'd')
+		{
+			printf("dsdsdf");		
+		}
+	if (hex_des == 'h')
+		{
+			printf("lololv");
+		}
+	return 0;
+}

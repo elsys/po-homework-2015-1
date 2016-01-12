e2f8331eaaacaf3d3c0ecc81ea6d9df6a76fea50
VelislavYotov <slaviy999@abv.bg>
2016-01-12 20:53:57 +0200

    Update task2.c

--- 2015-2016/A/05/03/task2.c
+++ 2015-2016/A/05/03/task2.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 int main()
 {
-    int lines,counter,temp,counter2;
-    printf("Input number of lines.\n");
-    scanf("%d",&lines);
+char string[100];
+int counter,lenght;
+scanf("%s",string);
 
-    for(counter=0;counter<=lines;counter++)
-    {
-        temp=lines-counter;
-        for(counter2=0;counter2<lines*2;counter2++)
-        {
-            if(counter2>=temp && lines*2-1-counter2>temp)
-                {
-			printf("+");
-		}
-            else
-                printf(" ");
-        }
-        printf("\n");
-    }
+printf("\n");
+lenght=strlen(string);
+	for(counter=lenght-1;counter>=0;counter--)
+    	    {
+        	printf("%c",string[counter]);
+    	    }
 }

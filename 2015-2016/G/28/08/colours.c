33dbba265280515ccdee4b06d7e802ff5f4ec4e0
Philip Pepegov <you@example.com>
2016-01-13 23:48:22 +0200

    пак не е цялада задача, а 1/8. Да не повярва човек,научих се с функциит поне.

--- 2015-2016/G/28/08/colours.c
+++ 2015-2016/G/28/08/colours.c
@@ -1,19 +1,41 @@
 #include<stdio.h>
 
+int hex_dec(char[]);
+int dec_hex(char[]);
+
+
 int main()
 {
 	char hex_des;
-	
+	char hex_str[100];
+
 	scanf("%c", &hex_des);
-	//printf("%c", hex_des);
 
 	if (hex_des == 'd')
 		{
-			printf("dsdsdf");		
+			scanf("%s",hex_str);
+			func(hex_str);
+
 		}
-	if (hex_des == 'h')
+	else if (hex_des == 'h')
 		{
-			printf("lololv");
+			scanf("%s",hex_str);
+			func2( hex_str);
 		}
 	return 0;
 }
+
+int func(char hex_dec[100])
+{
+
+
+    return 0;
+}
+
+int func2(char hex_dec[100])
+{
+
+   
+    return 0;
+}
+

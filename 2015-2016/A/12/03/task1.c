aee03c63cc600e5c65d7d4f0009c5ea41f7c9a2a
Ivaza <iva.v.zahova@gmail.com>
2016-01-14 22:39:12 +0200

    POPRAVKA na staro doma6no 03
    Поправка на старата поправка от преди 2 месеца. : P

--- 2015-2016/A/12/03/task1.c
+++ 2015-2016/A/12/03/task1.c
@@ -3,10 +3,7 @@ int main()
 {
     int rows,s,n,i;
     i=0;
-    printf("Enter rows: ");
     scanf("%d",&rows);
-    if(rows<0)
-        printf("Enter a bigger number for a triangle. ;)");
     for(n=1;n<=rows;n++)
     {
         for(s=1;s<=rows-n;s++)
@@ -15,14 +12,12 @@ int main()
         }
         while(i!=2*n-1)
         {
-            printf(". ");
+            printf("* ");
             i++;
 
         }
         i=0;
         printf("\n");
-        if(rows<2)
-            printf("Enter a bigger number for a prettier triangle. ;)");
     }
     return 0;
 }

95670893ab5f6184875385946602a215dbca0e3b
ivostefanov <ivostefanov.tues@gmail.com>
2016-01-18 23:50:03 +0200

    Homework06

---
+++ 2015-2016/A/14/06/task3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int i, j, c, n, k=0;
+    int arr[200], arr2[200]={0};
+
+    scanf("%d",&n);
+
+    for (i=0; i<n; ++i)
+    {
+        int nm, dp=0;
+        scanf("%d",&nm);
+        for(j=0; j<k; ++j)
+        {
+            if(arr[j]==nm)
+            {
+                arr2[j]=dp=1;
+                break;
+            }
+        }
+        if(!dp){
+            arr[k++]=nm;
+        }
+    }
+
+    for (c=i=0; i<k; ++i)
+    {
+        if(!arr2[i])printf("%d ",arr[c++]=arr[i]);
+    }
+    printf("\n");
+    return 0;
+}

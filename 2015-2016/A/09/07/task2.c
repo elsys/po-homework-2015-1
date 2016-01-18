c5b14e0b4bdb3541f50c4c1f2f885b60b49c6701
Dimitar Nikolov <dknikolov@gmail.com>
2016-01-18 22:20:43 +0000

    Add Homework 07

---
+++ 2015-2016/A/09/07/task2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, v, p;
+    scanf("%d", &n);
+    scanf("%d", &v);
+    scanf("%d", &p);
+    
+    if (v == 1) {
+        n |= 1 << p;
+    } else {
+        if (n & (1 << p) ? 1 : 0) {
+            n ^= 1 << p;
+        } else {
+            n |= 1 << p;
+        }
+    }
+    
+    printf("%d", n);
+    
+    return 0;
+}

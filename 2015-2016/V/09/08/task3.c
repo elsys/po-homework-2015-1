7685d3e1ee1945d99921b3dbbae1a9a1cbc25855
DeyanHristov <Deyan.Xristov@gmail.com>
2016-01-19 09:05:20 +0200

    Update task3.c

--- 2015-2016/V/09/08/task3.c
+++ 2015-2016/V/09/08/task3.c
@@ -4,11 +4,11 @@ int main()
   int n = 0, i, j, temp = 0;
   int deers[200];
   
-  printf("Enter amount of deers: \n");
+  //printf("Enter amount of deers: \n");
   do{
     scanf("%d", &n);
   }while(n<=0 || n>=200 || n%2==0);
-  printf("Enter the deer's tags: \n");
+  //printf("Enter the deer's tags: \n");
   for (i = 0; i < n; i++){
     scanf("%d", &deers[i]);
   }
@@ -23,6 +23,6 @@ int main()
     else
       temp = 0;
   }
-  printf("Rudolph's tag is %d \n", deers[i]);
+  printf("%d", deers[i]);
   return 0;
 }

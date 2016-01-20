b6bf986e083832a24530b6099125f4a670dfaf04
NikoletaDalaktchieva <nikoletadal@gmail.com>
2016-01-20 22:21:15 +0200

    Homework 07

---
+++ 2015-2016/B/25/07/task2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, v=2, p=8;
+    while((p<0||p>7)&&(v!=1||v!=0)){
+    scanf("%d %d %d", &n, &v, &p);}
+    printf("%d", n^(1<<p));
+    return 0;
+}

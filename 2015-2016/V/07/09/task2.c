1ce934c59c117c034d85f5b673e44cc8bd36f5c9
Vladomaimun <vladimir.garistov42@gmail.com>
2016-01-21 02:45:39 +0200

    domashno 09 i drebna promiana na osmo

---
+++ 2015-2016/V/07/09/task2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, v, p;
+	scanf("%d",&n);
+	do
+	{
+		scanf("%d",&v);
+	}
+	while(v!=0 && v!=1);
+	do
+	{
+		scanf("%d",&p);
+	}
+	while(p<0 || p>7);
+	if(v==1)
+		printf("%d\n",n | (1 << p));
+	else
+		printf("%d\n",n ^ (1 << p));
+	return 0;
+}

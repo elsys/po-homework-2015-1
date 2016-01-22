1d79def2d26604a23cf5a95aae3637ed50c8c7fc
iordanivanov <iiordan.ivanov@gmail.com>
2016-01-22 23:42:36 +0200

    Create task3.c

---
+++ 2015-2016/A/16/07/task3.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, t;
+    int palindromflag = 0;
+    int x[50];
+    int temp, counter, counter2;
+    scanf("%d", &n);
+    t = n;
+    counter = 0;
+    counter2 = 0;
+    while(palindromflag==0)
+    {
+        while(n > 0)
+        {
+            temp = n % 16;
+            if(n>=0||n<=9)
+            {
+                x[counter] = 48 + temp;
+            }
+            if(n>=10||n<=16)
+            {
+                switch(temp)
+                {
+                case 10:
+                    x[counter] = 'A';
+                    break;
+                case 11:
+                    x[counter] = 'B';
+                    break;
+                case 12:
+                    x[counter] = 'C';
+                    break;
+                case 13:
+                    x[counter] = 'D';
+                    break;
+                case 14:
+                    x[counter] = 'E';
+                    break;
+                case 15:
+                    x[counter] = 'F';
+                    break;
+                }
+            }
+            counter++;
+            n = n / 16;
+        }
+        counter--;
+        counter2 = 0;
+        while(counter>-1)
+        {
+            if(x[counter] == x[counter2])
+            {
+                palindromflag = 1;
+                printf("%c", x[counter]);
+            }
+            if(x[counter] != x[counter2])
+            {
+                palindromflag = 0;
+            }
+            counter--;
+            counter2++;
+        }
+        counter = 0;
+        t++;
+        n = t;
+    }
+    return 0;
+}

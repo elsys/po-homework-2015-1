283beb79d25f75272ba82cc475d39ccfbf632948
VelislavVelikov <velislavveliikov2@gmail.com>
2016-01-22 21:54:50 +0200

    Homework 07

---
+++ 2015-2016/B/06/07/task1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    while (a<0 || a>7) scanf("%d",&a);
+    scanf("%d",&b);
+    while (b<0 || b>200) scanf("%d",&b);
+    if (((b >> a) & 1) == 1) printf ("1");
+    if (((b >> a) & 1) == 0) printf ("0");
+    return 0;
+}
+

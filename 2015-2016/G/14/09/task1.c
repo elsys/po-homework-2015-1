fa19dee2d3379b88aca0867f2962354d40017b86
evgeniq <evgeniq.grigorova99@gmail.com>
2016-01-22 18:54:53 +0200

    Evgeniq Grigorova N14 10g

---
+++ 2015-2016/G/14/09/task1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define CHECK_BIT(var,pos) (((var)>>(pos)) & 1)
+
+int main(){
+    int a, b;
+    do {
+        scanf("%d %d", &a, &b);
+    } while((0 > a) || (a > 7) || (0 > b) || (b > 200));
+
+    printf("%d", CHECK_BIT(b, a));
+
+    return 0;
+}

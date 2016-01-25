78a921ae2aa9314c68d09f02f8b1202303410787
DeyanHristov <Deyan.Xristov@gmail.com>
2016-01-25 12:40:44 +0200

    Update task2.c

--- 2015-2016/V/09/06/task2.c
+++ 2015-2016/V/09/06/task2.c
@@ -4,22 +4,18 @@ int exact_square(int);
 int main() {
     int n;
 
-    //printf("Enter number: \n");
     scanf("%d", &n);
-
     if(exact_square(n) == 1) {
         printf("1");
     }
     else {
         printf("0");
     }
-
     return 0;
 }
 
 int exact_square(int n) {
     int i;
-
     for(i = 0; i <= n; i++) {
         if (n == i * i) {
             return 1;

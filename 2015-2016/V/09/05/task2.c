7db235c87a2a79591b2b7a0f6af4540da0cc47fa
DeyanHristov <Deyan.Xristov@gmail.com>
2016-01-25 12:36:26 +0200

    Update task2.c

--- 2015-2016/V/09/05/task2.c
+++ 2015-2016/V/09/05/task2.c
@@ -5,25 +5,20 @@ int main()
    int x[4][4];
    int y[4][4];
    
-   //printf("Enter 16 numbers:");
    for (i = 0; i < 4; i++){
       for(j = 0; j < 4; j++){
          scanf("%d",&x[i][j]);
       }
    }
-
    for (i = 0; i < 4; i++){
       for( j = 0 ; j < 4; j++ ){
          y[j][i] = x[i][j];
       }
    }
-
    for (i = 0; i < 4; i++) {
       for (j = 0; j < 4; j++){
          printf("%d ",y[i][j]);
       }
-      printf("\n");
    }
-
    return 0;
 }
